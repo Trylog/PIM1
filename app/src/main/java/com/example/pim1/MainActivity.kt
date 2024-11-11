db35103f355a6907463fb2ffec40069fee563f27
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2024-11-11 14:33:11 +0100

    finished ver 1

---
+++ app/src/main/java/com/example/pim1/MainActivity.kt
@@ -0,0 +1,357 @@
+//Michał Bernacki-Janson
+package com.example.pim1
+
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuItemColors
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import com.example.pim1.ui.theme.AppTheme
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import kotlin.math.exp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Tabs()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Tabs(modifier: Modifier = Modifier){
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+    val titles = listOf("Różnica dat", "Dodaj lub odejmij dni")
+
+    Column (
+        modifier = Modifier.padding(top = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        //Surface(Modifier.height(40.dp)){}
+        TabRow(selectedTabIndex = selectedTabIndex,
+            contentColor = Color.Black,
+            indicator = { tabPositions ->
+                SecondaryIndicator(
+                    Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                    color = Color(0xFF65558F)
+                )
+
+            }) {
+            titles.forEachIndexed{
+                index, title -> Tab(
+                    text = { Text(title)},
+                    selected = (index == selectedTabIndex),
+                    onClick = {selectedTabIndex = index}
+                )
+            }
+        }
+        when(selectedTabIndex){
+            0 -> DateDifference()
+            1 -> AddDays()
+        }
+    }
+}
+
+@Composable
+fun DatePickerTextField(text : String, selectedDate: String, onDataChange: (String) -> Unit, modifier: Modifier = Modifier){
+
+    //var selectedDate by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    val calendar = Calendar.getInstance()
+    var isFocused by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+
+    val datePicker = android.app.DatePickerDialog(
+        context,
+        { _, year, month, dayOfMonth ->
+            onDataChange(if(dayOfMonth > 9){
+                if(month > 9){
+                    "${dayOfMonth}/${month + 1}/$year"
+                }else{
+                    "${dayOfMonth}/0${month + 1}/$year"
+                }
+            } else if(month > 9){
+                "0${dayOfMonth}/${month + 1}/$year"
+            }else{
+                "0${dayOfMonth}/0${month + 1}/$year"
+            })
+        },
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH),
+        calendar.get(Calendar.DAY_OF_MONTH)
+    )
+
+    OutlinedTextField(
+        value = selectedDate,
+        onValueChange = {},
+        label = { Text(text) },
+        readOnly = true,
+        modifier = Modifier
+            .padding(top = 10.dp)
+            .onFocusChanged { focusState ->
+                if (focusState.hasFocus) {
+                    isFocused = true
+                    datePicker.show()
+                    focusManager.clearFocus()
+                }
+            }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateOutput(text : String, modifier: Modifier = Modifier){
+    TextField(
+        value = text,
+        onValueChange = {},
+        readOnly = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            containerColor = Color.LightGray,
+            unfocusedBorderColor = Color(0xFF65558F),
+            focusedBorderColor = Color(0xFF65558F)
+            ),
+        modifier = Modifier.padding(top = 20.dp),
+    )
+}
+
+fun dateDifferenceOutput(start: String, end: String): String{
+    val dateFormater = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    var outputString = ""
+    if(start.isNotBlank() && end.isNotBlank()) {
+        val startDate = LocalDate.parse(start, dateFormater)
+        val endDate = LocalDate.parse(end, dateFormater)
+        val period = Period.between(startDate, endDate)
+        if(period.years > 0) {
+            outputString += if (period.years < 4) {
+                if (period.years == 1) {
+                    "${period.years} rok, "
+                } else {
+                    "${period.years} lata, "
+                }
+            } else {
+                "${period.years} lat, "
+            }
+        }
+
+        outputString += if (period.months < 4) {
+            if (period.months == 1) {
+                "${period.months} miesiąc, "
+            } else {
+                "${period.months} miesiące, "
+            }
+        } else {
+            "${period.months} miesięcy, "
+        }
+
+        outputString += if (period.days == 1){
+            "${period.days} dzień"
+        }else{
+            "${period.days} dni"
+        }
+
+        //in other units
+
+        outputString += "\n"
+
+        val years = ChronoUnit.YEARS.between(startDate, endDate).toInt()
+        if(years > 0) {
+            outputString += if (years < 4) {
+                if (years == 1) {
+                    "$years rok lub "
+                } else {
+                    "$years lata lub "
+                }
+            } else {
+                "$years lat lub "
+            }
+        }
+
+        val months = ChronoUnit.MONTHS.between(startDate, endDate).toInt()
+        if(months > 0) {
+            outputString += if (months < 4) {
+                if (months == 1) {
+                    "$months miesiąc lub "
+                } else {
+                    "$months miesiące lub "
+                }
+            } else {
+                "$months miesięcy lub "
+            }
+        }
+
+        val days = ChronoUnit.DAYS.between(startDate, endDate).toInt()
+        outputString += if (days == 1){
+            "$days dzień"
+        }else{
+            "$days dni"
+        }
+
+    }else ""
+    return outputString
+}
+
+@Composable
+fun DateDifference(modifier: Modifier = Modifier){
+    var start by remember { mutableStateOf("") }
+    var end by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 106.dp, bottom = 156.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        DatePickerTextField("Początek", start, {n -> start = n})
+        DatePickerTextField("Koniec", end, {n -> end = n})
+        val dateFormater = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        DateOutput(dateDifferenceOutput(start, end))
+    }
+}
+
+
+
+@Composable
+fun AddDays(modifier: Modifier = Modifier){
+    val focusManager = LocalFocusManager.current
+    var start by remember { mutableStateOf("") }
+    Box(modifier = Modifier.pointerInput(Unit) {
+        detectTapGestures(
+            onTap = {
+                focusManager.clearFocus()
+            }
+        )
+    }) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 106.dp, bottom = 156.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            DatePickerTextField("Początek", start, { n -> start = n })
+            var expanded by remember { mutableStateOf(false) }
+            val focusManager2 = LocalFocusManager.current
+            var pickedText by remember { mutableStateOf("Dodaj") }
+            Box {
+                OutlinedTextField(
+                    value = pickedText,
+                    readOnly = true,
+                    onValueChange = {},
+                    modifier = Modifier.onFocusChanged { focusState ->
+                        if (focusState.hasFocus) {
+                            expanded = true
+                            focusManager2.clearFocus()
+                        }
+                    }
+                )
+                if (expanded) {
+                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false },  modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                        DropdownMenuItem(
+                            text = { Text("Dodaj") },
+                            onClick = { pickedText = "Dodaj"; expanded = false },
+
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Odejmij") },
+                            onClick = { pickedText = "Odejmij"; expanded = false }
+                        )
+                    }
+                }
+            }
+            var days by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = days,
+                onValueChange = { days = it },
+                label = { Text("Liczba dni") },
+                singleLine = true,
+                modifier = modifier
+
+            )
+            val dateFormater = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            if (pickedText == "Dodaj") {
+                DateOutput(
+                    if (start.isNotBlank() && days.isNotBlank())
+                        LocalDate.parse(start, dateFormater)
+                            .plusDays(days.toLong()).format(dateFormater).toString() else ""
+                )
+            } else {
+                DateOutput(
+                    if (start.isNotBlank() && days.isNotBlank())
+                        LocalDate.parse(start, dateFormater)
+                            .minusDays(days.toLong()).format(dateFormater).toString() else ""
+                )
+            }
+        }
+    }
+}
