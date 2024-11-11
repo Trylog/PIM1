859f31f65fae4ffc167ac3cea06c3ce41d0a3673
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2024-11-11 15:59:23 +0100

    finished ver 1.01

--- app/src/main/java/com/example/pim1/MainActivity.kt
+++ app/src/main/java/com/example/pim1/MainActivity.kt
@@ -3,41 +3,28 @@ package com.example.pim1
 
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.os.Message
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.SecondaryIndicator
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -50,22 +37,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat
 import com.example.pim1.ui.theme.AppTheme
 import java.time.LocalDate
 import java.time.Period
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -108,11 +87,11 @@ fun Tabs(modifier: Modifier = Modifier){
 
             }) {
             titles.forEachIndexed{
-                index, title -> Tab(
-                    text = { Text(title)},
-                    selected = (index == selectedTabIndex),
-                    onClick = {selectedTabIndex = index}
-                )
+                    index, title -> Tab(
+                text = { Text(title)},
+                selected = (index == selectedTabIndex),
+                onClick = {selectedTabIndex = index}
+            )
             }
         }
         when(selectedTabIndex){
@@ -166,6 +145,7 @@ fun DatePickerTextField(text : String, selectedDate: String, onDataChange: (Stri
                 }
             }
     )
+    var showDialog by remember { mutableStateOf(false) }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -179,7 +159,7 @@ fun DateOutput(text : String, modifier: Modifier = Modifier){
             containerColor = Color.LightGray,
             unfocusedBorderColor = Color(0xFF65558F),
             focusedBorderColor = Color(0xFF65558F)
-            ),
+        ),
         modifier = Modifier.padding(top = 20.dp),
     )
 }
@@ -321,7 +301,7 @@ fun AddDays(modifier: Modifier = Modifier){
                             text = { Text("Dodaj") },
                             onClick = { pickedText = "Dodaj"; expanded = false },
 
-                        )
+                            )
                         DropdownMenuItem(
                             text = { Text("Odejmij") },
                             onClick = { pickedText = "Odejmij"; expanded = false }
